48ca9880c8dd90114b0ca50ccc4884a582c3af2b
marcusviniciusls <viniciusmls@outlook.com>
2021-04-21 15:41:32 -0300

    Classe Client criado com sucesso.

---
+++ src/br/com/udemy/entities/Client.java
@@ -0,0 +1,40 @@
+package br.com.udemy.entities;
+
+import java.util.Date;
+
+public class Client {
+
+	// Atributos
+	private String name;
+	private String email;
+	private Date birthDate;
+	
+	// Contrutores
+	public Client(String name, String email, Date birthDate) {
+		this.name = name;
+		this.email = email;
+		this.birthDate = birthDate;
+	}
+	public Client() {}
+	
+	// Métodos Getteres e Setteres
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}	
+	
+}
