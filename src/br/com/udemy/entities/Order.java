a60ad80ef6f8b7953ac89afac2434afc8a8cc08c
marcusviniciusls <viniciusmls@outlook.com>
2021-04-21 16:12:01 -0300

    Todas as classes criadas

---
+++ src/br/com/udemy/entities/Order.java
@@ -0,0 +1,64 @@
+package br.com.udemy.entities;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
+import br.com.udemy.enums.OrderStatus;
+
+public class Order {
+
+	// Atributos
+	private Date moment;
+	private OrderStatus status;	
+	private List<OrdemItem> items = new ArrayList<>();
+	
+	// Outros métodos
+	public void addItem(OrdemItem item) {
+		items.add(item);
+	}
+	public void removeItem(OrdemItem item) {
+		items.remove(item);
+	}
+	public Double total() {
+		Double sumTotal = 0.0;
+		for(OrdemItem e: items) {
+			sumTotal+= e.subTotal();
+		}		
+		if(sumTotal > 0) {
+			return sumTotal;
+		}
+		else {
+			JOptionPane.showInternalMessageDialog(null, "Não há valor para esse pedido");
+			return 0.0;
+		}
+		
+	}
+	
+	// Construtores
+	public Order(Date moment, OrderStatus status) {
+		this.moment = moment;
+		this.status = status;
+	}
+	public Order() {}
+	
+	// Métodos Getteres and Setteres
+	public Date getMoment() {
+		return moment;
+	}
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+	public OrderStatus getStatus() {
+		return status;
+	}
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+	
+	
+	
+	
+}
---
+++ src/br/com/udemy/enums/OrderStatus.java
@@ -0,0 +1,10 @@
+package br.com.udemy.enums;
+
+public enum OrderStatus {
+
+	PENDING_PAYMENT,
+	PROCESSING,
+	SHIPPED,
+	DELIVERED
+	
+}
